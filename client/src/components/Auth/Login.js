470c9cbf4cbde7d5901af5c040d61e25dff06612
Saul M <fob2257@gmail.com>
2019-10-07 10:51:25 -0700

    User can authenticate with Google provider

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,18 +1,43 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+import { meQuery } from '../../graphql/queries';
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+
+  const onSuccess = async googleUser => {
+    const { id_token: idToken } = googleUser.getAuthResponse();
+
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken },
+    });
+
+    const response = await client.request(meQuery);
+
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId='17846108568-h45esd9trs7ko8l0bi2m4cdqqgklqrs7.apps.googleusercontent.com'
+        onSuccess={onSuccess}
+        isSignedIn
+      />
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
