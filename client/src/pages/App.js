17709394f75ab6ffc8dbdc2330af01ef6277e181
Saul M <fob2257@gmail.com>
2019-10-07 15:36:44 -0700

    Header component

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import withRoot from "../withRoot";
 
-const App = () => {
-  return <div>App</div>;
-};
+import withRoot from '../withRoot';
+import Header from '../components/Header';
+
+const App = () => (
+  <React.Fragment>
+    <Header />
+  </React.Fragment>
+);
 
 export default withRoot(App);
