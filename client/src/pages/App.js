83aedc02aad033553710d4cb02350e4a30fe8583
Saul M <fob2257@gmail.com>
2019-10-07 16:13:16 -0700

    Showing map on app page

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import withRoot from '../withRoot';
 import Header from '../components/Header';
+import Map from '../components/Map';
 
 const App = () => (
   <React.Fragment>
     <Header />
+    <Map />
   </React.Fragment>
 );
 
